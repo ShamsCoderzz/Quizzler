629b3773d7238fa8a687690981c7a53df711d5e7
muhammadshams <mr.shamssheikh@gmail.com>
2020-02-23 21:01:23 +0500

    adding question

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var question: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueBtn: UIButton!
+    @IBOutlet weak var falseBtn: UIButton!
+    
+    let quiz = [
+        ["Approximately one quarter of human bones are in the feet.","True"],
+        ["A slug's blood is green.","False"],
+        ["he total surface area of two human lungs is approximately 70 square metres.","True"],
+        ["West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.","True"]
+    ]
+    
+    var questionNumber = 0 ;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        makeQuiz()
     }
 
-
+    @IBAction func answerBtnPressed(_ sender: UIButton) {
+            
+            let userAnswer = sender.currentTitle
+            let actualAnswer = quiz[questionNumber][1]
+            
+            if userAnswer == actualAnswer {
+                print("Right")
+            }else {
+                print("Wrong")
+            }
+        if  questionNumber < quiz.count {
+            makeQuiz()
+            questionNumber+=1;
+        } else {
+            print("finished quiz")
+                
+        }
+    }
+    
+    func makeQuiz(){
+        let totalQuestion : Float = Float(quiz.count)
+        let attemptedquestion : Float = Float(questionNumber)
+        let persentage : Float = Float(totalQuestion * 100 / attemptedquestion)
+        question.text = quiz[questionNumber][0]
+        progressBar.progress = persentage
+    }
 }
 
